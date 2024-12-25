772aa756a07491cb837b00a0b75f04151c6e9751
SunnyLo <25066078+LoSunny@users.noreply.github.com>
2024-12-25 13:13:45 +0800

    Tidal update

--- Tidal/Tidal.js
+++ Tidal/Tidal.js
@@ -1,6 +1,7 @@
 // https://github.com/Yuheng0101/X/blob/main/Scripts/tidal.js
 
 console.log('hello');
-console.log(Object.keys($request));
+console.log(Object.entries($request));
+console.log($request);
 console.log($request.url);
 console.log('End');
