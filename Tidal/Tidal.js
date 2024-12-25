100daa2002cb40247b86a4a8ad1c5ce7368b38dc
SunnyLo <25066078+LoSunny@users.noreply.github.com>
2024-12-25 13:34:43 +0800

    Tidal update

--- Tidal/Tidal.js
+++ Tidal/Tidal.js
@@ -1,13 +1,9 @@
 // https://github.com/Yuheng0101/X/blob/main/Scripts/tidal.js
 
 console.log('hello1');
-console.log(Object.entries($request));
-console.log('hello2');
 console.log(JSON.stringify($request));
-console.log('hello3');
+console.log('hello2');
 console.log($request.url);
-console.log('hello4');
-for (let a in Object.keys($request)) {
-  console.log('Bye', a, $request[a]);
-}
+console.log('hello3');
+console.log(new URLSearchParams($request.url).get('audioquality'));
 console.log('End');
